d7ecc4cc677aeedfdddbed24ac579775d9668c58
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2018-08-07 01:20:59 +0900

    add count

--- osero.py
+++ osero.py
@@ -3,16 +3,18 @@ from pygame.locals import *
 import sys
 import numpy as np
 
-SCREEN_SIZE = (500,500);
+SCREEN_SIZE = (750,500);
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("osero")
 void_position  =0
 brack_position =1
 white_position =2
+brack_num=white_num=all_number=0
 green=(55,255,55)
 white=(255,255,255)
 brack=(0,0,0)
+purple=(155,155,255)
 position = np.zeros((8,8),dtype=int)
 position[3,3]=position[4,4]=position[2,3]=position[5,4]=position[4,5]=position[3,2]=1
 position[3,4]=position[4,3]=2
@@ -20,15 +22,14 @@ position[0,0]=position[7,7]=1
 #back_img = pygame.image.load("./back.png").convert()
 def initial_draw(screen):
     pygame.display.update()
-    pygame.draw.rect(screen, (white), Rect(0,0,500,500),0)
+    pygame.draw.rect(screen, (white), Rect(0,0,750,500),0)
     pygame.draw.rect(screen, (green), Rect(10,10,500-15,500-15),0)
     pygame.draw.rect(screen, (brack), Rect(10,10,500-15,500-15),5)
-
+    pygame.draw.rect(screen, (purple), Rect(510,20,230,450),0)
     pygame.draw.circle(screen, (brack), (135,135), 8)
     pygame.draw.circle(screen, (brack), (135+240,135), 8)
     pygame.draw.circle(screen, (brack), (135,135+240), 8)
     pygame.draw.circle(screen, (brack), (135+240,135+240), 8)
-    
     for i in range(8):
         pygame.draw.line(screen, (brack), (15+60*(i+1),15), (15+60*(i+1),485),3)
         pygame.draw.line(screen, (brack), (15,15+60*(i+1)), (485,15+60*(i+1)),3)
@@ -42,6 +43,14 @@ def koma_draw(screen):
                 pygame.draw.circle(screen, (brack), (45+60*(i),45+60*(j)), 25)
             if position[i,j]==2:
                 pygame.draw.circle(screen, (white), (45+60*(i),45+60*(j)), 25)
+def count_koma():
+    brack_num=white_num=all_number=0
+    for i in range(8):
+        for j in range(8):
+            if position[i,j]==brack_position:brack_num+=1
+            elif position[i,j]==white_position:white_num+=1
+    all_number = [white_num,brack_num]
+    print(str(all_number[0])+"-"+str(all_number[1]))
 
 def mouse_get_position():
     mouse_pressed = pygame.mouse.get_pressed()
@@ -162,5 +171,5 @@ while game_mode:
                     if mouse_position[0]>(20+60*i) and mouse_position[0]<(60+60*i) and mouse_position[1]>(25+60*j) and mouse_position[1]<(60+60*j):
                         position[i,j]=2
                         last_position=[i,j]
-                        print(last_position)
                         reverse(last_position)
+                        count_koma()
