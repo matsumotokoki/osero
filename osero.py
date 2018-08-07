9929e91e9a600af4f8363bb4fbe249ee9549ef6d
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2018-08-07 11:38:22 +0900

    cross reverse

--- osero.py
+++ osero.py
@@ -15,9 +15,10 @@ green=(55,255,55)
 white=(255,255,255)
 brack=(0,0,0)
 purple=(155,155,255)
+switch=1
 position = np.zeros((8,8),dtype=int)
-position[3,3]=position[4,4]=position[2,3]=position[5,4]=position[4,5]=position[3,2]=1
-position[3,4]=position[4,3]=2
+position[3,3]=position[4,4]=position[2,3]=position[5,4]=position[4,5]=position[3,2]=position[2,5]=position[3,2]=1
+position[3,4]=position[4,3]=position[5,3]=position[3,5]=2
 position[0,0]=position[7,7]=1
 #back_img = pygame.image.load("./back.png").convert()
 def initial_draw(screen):
@@ -59,10 +60,7 @@ def mouse_get_position():
         return(x,y)
 
 def reverse(last_position):
-    right_frag=True 
-    left_frag=True
-    up_frag=True
-    down_frag=True
+    right_frag=left_frag=up_frag=down_frag=right_up_frag=right_down_frag=left_up_frag=left_down_frag=True
     position_x=last_position[0]
     position_y=last_position[1]
     while right_frag:
@@ -149,6 +147,98 @@ def reverse(last_position):
             position_y=last_position[1]
             break
 
+    while right_up_frag:
+        if position_x==7 or position_y==0:
+            right_up_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+        if position[position_x+1,position_y-1]==1:
+            position_x+=1
+            position_y-=1
+        elif position[position_x+1,position_y-1]==2:
+            for reverse_time in range(last_position[0]+1,position_x+1):
+                position[reverse_time,last_position[1]-1]=2
+                last_position[1]-=1
+            right_up_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+        elif position[position_x+1,position_y-1]==0 or position_x>8 or position_y<0:
+            right_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+
+    while right_down_frag:
+        if position_x==7 or position_y==7:
+            right_up_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+        if position[position_x+1,position_y+1]==1:
+            position_x+=1
+            position_y+=1
+        elif position[position_x+1,position_y+1]==2:
+            for reverse_time in range(last_position[0]+1,position_x+1):
+                position[reverse_time,last_position[1]+1]=2
+                last_position[1]+=1
+            right_up_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+        elif position[position_x+1,position_y+1]==0 or position_x>8 or position_y>8:
+            right_up_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+
+    while left_up_frag:
+        if position_x==0 or position_y==0:
+            left_up_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+        if position[position_x-1,position_y-1]==1:
+            position_x-=1
+            position_y-=1
+        elif position[position_x-1,position_y-1]==2:
+            for reverse_time in range(position_x,last_position[0]):
+                position[reverse_time,last_position[1]-1]=2
+                last_position[1]-=1
+            left_up_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+        elif position[position_x-1,position_y-1]==0 or position_x<0 or position_y<0:
+            left_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+
+    while left_down_frag:
+        if position_x==0 or position_y==8:
+            left_down_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+        if position[position_x-1,position_y+1]==1:
+            position_x-=1
+            position_y+=1
+        elif position[position_x-1,position_y+1]==2:
+            for reverse_time in range(position_x,last_position[0]):
+                position[reverse_time,last_position[1]-1]=2
+                last_position[1]+=1
+            left_down_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+        elif position[position_x-1,position_y+1]==0 or position_x<0 or position_y<0:
+            left_down_frag=False
+            position_x=last_position[0]
+            position_y=last_position[1]
+            break
+
 game_mode = True
 
 while game_mode:
@@ -164,12 +254,18 @@ while game_mode:
                 sys.exit()
             if event.key == K_SPACE:
                 koma_draw(screen)
+                print(position)
         if event.type == MOUSEBUTTONDOWN and event.button == 1:
             mouse_position=mouse_get_position()
             for i in range(8):
                 for j in range(8):
                     if mouse_position[0]>(20+60*i) and mouse_position[0]<(60+60*i) and mouse_position[1]>(25+60*j) and mouse_position[1]<(60+60*j):
-                        position[i,j]=2
                         last_position=[i,j]
-                        reverse(last_position)
+                        if switch == 1:
+                            position[i,j]=2
+                            switch = 0
+                            reverse(last_position)
+                        elif switch == 0:
+                            position[i,j]=1
+                            switch =1
                         count_koma()
